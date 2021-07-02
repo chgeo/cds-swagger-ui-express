336addfe98f828165534a977a560594460123b37
Christian Georgi <christian.georgi@sap.com>
2021-07-02 19:53:08 +0200

    Polyfill for older cds-dk, logging

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const cds = require ('@sap/cds-dk')
+const cds = _require ('@sap/cds-dk', orPolyfillCompileOpenAPI)
 const swaggerUi = require('swagger-ui-express')
 const express = require('express')
-
-const debug = cds.debug('openapi')
-const cache={}
-
 
 module.exports = (options={basePath:'/$api-docs'}) => {
   const router = express.Router()
   cds.on ('serving', service => {
     if (!isOData (service))  return
-    debug && debug (`Serving ${service.name} at ${apiPath}`)
     const apiPath = options.basePath+service.path
     const mount = apiPath.replace('$','[\\$]')
+    console.log ('[cds] - serving Swagger UI for ', {service: service.name, at: apiPath})
     router.use(mount, (req, _, next) => {
       req.swaggerDoc = toOpenApiDoc(service, options)
       next()
@@ -22,9 +18,9 @@ module.exports = (options={basePath:'/$api-docs'}) => {
   return router
 }
 
+const cache={}
 function toOpenApiDoc(service, options={}) {
   if (!cache[service.name]) {
-    debug && debug ('Compiling Open API spec for', service.name)
     cache[service.name] = cds.compile.to.openapi (service.model, {
       service: service.name,
       'openapi:url': service.path,
@@ -45,3 +41,33 @@ function addLinkToIndexHtml(service, apiPath) {
 function isOData(service) {
   return Object.keys(service._adapters) .find (a => a.startsWith ('odata'))
 }
+
+function _require(id, _else) {
+  try {
+    return require (id)
+  } catch (e) {
+    return _else()
+  }
+}
+
+// polyfill for @sap/cds-dk < 4.3
+function orPolyfillCompileOpenAPI() {
+  const cds = require ('@sap/cds')
+  const get_compile = Reflect.getOwnPropertyDescriptor(cds,'compile').get
+
+  // extending cds to lazy-load additional submodules provided by cds-dk
+  return Object.defineProperties (cds, {
+    compile: {enumerable:true, get:()=> {
+      // lazy-extending cds.compile.to only when cds.compile getter is invoked
+      // otherwise we might cause cds.env being invoked too early.
+      const compile = get_compile.call(cds)
+      Object.defineProperties (compile.to, {
+        openapi: {enumerable:true, get:()=> require('@sap/cds-dk/lib/compile/openapi')}
+      })
+      return compile
+    }},
+  })
+
+}
+
+/* eslint no-console:off */
